4cbe1fde5dc83fd3f112343d4ca4852fdc913fac
Daniel <danielmpang@gmail.com>
2023-10-21 16:39:51 -0500

    starting js automation

--- src/js/World.js
+++ src/js/World.js
@@ -1,15 +1,30 @@
+console.log('hi')
+
 const fs = require('fs');
 const csv = require('csv-parser');
 
+const acronym_table = [];
+
+fs.createReadStream('data.csv')
+  .pipe(csv())
+  .on('data', (row) => {
+    // Process each row from the CSV file and add it to the 2D array
+    acronym_table.push(Object.values(row));
+  })
+  .on('end', () => {
+    // The CSV parsing is complete, and the data is in the dataTable.
+    console.log(acronym_table);
+  });
+
 const gElement = doc.querySelector('#au'); // Replace 'au' with the actual id
 
-// Create an <a> element
-const aElement = doc.createElement('a');
-aElement.setAttribute('xlink:title', 'Australia');
-aElement.setAttribute('xlink:href', 'https://en.wikipedia.org/wiki/Australia');
-aElement.textContent = 'Australia';
+// // Create an <a> element
+// const aElement = doc.createElement('a');
+// aElement.setAttribute('xlink:title', 'Australia');
+// aElement.setAttribute('xlink:href', 'https://en.wikipedia.org/wiki/Australia');
+// aElement.textContent = 'Australia';
 
-// Append the <a> element to the <g> element
-gElement.appendChild(aElement);
+// // Append the <a> element to the <g> element
+// gElement.appendChild(aElement);
 
 
