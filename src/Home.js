0e88a50ee68267098819b01300c6fdebd7b82786
Kevin Miao <kevmiao2@gmail.com>
2023-10-22 02:24:16 -0500

    dropdown

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,11 @@
 import SvgComponent from "./Svgcomponent";
+import YearBox from "./YearBox";
 
 
 const Home = () => {
     return (
         <div className="home">
+            <YearBox />
             <SvgComponent/>
         </div>
     );
--- src/YearBox.js
+++ src/YearBox.js
@@ -1,12 +1,27 @@
-import DropdownMenu from "./DropdownMenu";
+import { useState } from 'react';
 
 const YearBox = () => {
+    const [open, setOpen] = useState(false);
+
+    const handleOpen = () => {
+        setOpen(!open);
+    };
+
     return (
-        <div className="yearBox">
-            <DropdownMenu />
-            <h2>Homepage</h2>
+        <div>
+            <button onClick={handleOpen}>Year</button>
+            {open ? (
+                <ul className="menu">
+                    <li className="menu-item">
+                        <button>2021</button>
+                    </li>
+                    <li className="menu-item">
+                        <button>2020</button>
+                    </li>
+                </ul>
+            ) : null}
         </div>
     );
-}
- 
+};
+
 export default YearBox;
