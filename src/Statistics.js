7aa51bd2e4dd420102ca38bc49394ccfb452ecd7
Bo Han <2005bhan@gmail.com>
2023-10-22 00:53:22 -0500

    First Image

--- src/Statistics.js
+++ src/Statistics.js
@@ -1,7 +1,9 @@
+import img1 from "./Images/TopArtist.png";
 const Statistics = () => {
     return (
         <div className="statistics">
             <h2>add here</h2>
+            <img src={img1} alt=""/>
         </div>
     );
 }
