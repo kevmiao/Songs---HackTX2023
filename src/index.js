eaa6acc8edc61c93c374e331d518c3f93458e48f
Kevin Miao <kevmiao2@gmail.com>
2023-10-22 00:47:29 -0500

    fixed-map

--- src/index.js
+++ src/index.js
@@ -10,31 +10,31 @@ root.render(
   </React.StrictMode>
 );
 
-async function fetchData() {
-  const result = await fetch('../data/mappingAcronyms.csv');
-  const data = await result.text();
-  return data;
-}
+// async function fetchData() {
+//   const result = await fetch('../data/mappingAcronyms.csv');
+//   const data = await result.text();
+//   return data;
+// }
 
-async function main() {
-  let acronym = await fetchData();
-  console.log(acronym); // This will be the CSV data as a string
+// async function main() {
+//   let acronym = await fetchData();
+//   console.log(acronym); // This will be the CSV data as a string
 
-  const gElement = document.getElementById('au'); // Replace 'au' with the actual id
-  const aElement = document.createElement('a');
-  aElement.setAttribute('xlink:title', 'Australia'); // Replace with your desired URL
-  aElement.setAttribute('xlink:href', 'https://example.com'); // Replace with your desired URL
+//   const gElement = document.getElementById('au'); // Replace 'au' with the actual id
+//   const aElement = document.createElement('a');
+//   aElement.setAttribute('xlink:title', 'Australia'); // Replace with your desired URL
+//   aElement.setAttribute('xlink:href', 'https://example.com'); // Replace with your desired URL
 
-  while (gElement.firstChild) {
-      aElement.appendChild(gElement.firstChild);
-    }
+//   while (gElement.firstChild) {
+//       aElement.appendChild(gElement.firstChild);
+//     }
     
-    // Append the <a> element to <g>
-    gElement.appendChild(aElement);
+//     // Append the <a> element to <g>
+//     gElement.appendChild(aElement);
 
-}
+// }
 
-main()
+// main()
 
 
 // If you want to start measuring performance in your app, pass a function
