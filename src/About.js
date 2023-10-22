66a40c11d6244aa393b8e17e113865f543e5e8bf
Kevin Miao <kevmiao2@gmail.com>
2023-10-22 01:48:40 -0500

    added about and deleted h1 from stats

---
+++ src/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+    return (
+        <div className="about">
+            <h1>About</h1>
+            <p>Our HackTX 2023 project.</p>
+        </div>
+    );
+}
+ 
+export default About;
--- src/Statistics.js
+++ src/Statistics.js
@@ -2,7 +2,6 @@ import img1 from "./Images/TopArtist.png";
 const Statistics = () => {
     return (
         <div className="statistics">
-            <h2>add here</h2>
             <img src={img1} alt=""/>
         </div>
     );
