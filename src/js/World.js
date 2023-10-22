0f1d2f510852e01bf6a31efb89a88f2dc6f1e3c1
Daniel <danielmpang@gmail.com>
2023-10-22 00:47:56 -0500

    no

--- src/js/World.js
+++ src/js/World.js
@@ -1,28 +1,28 @@
-async function fetchData() {
-    const result = await fetch('../data/mappingAcronyms.csv');
-    const data = await result.text();
-    return data;
-}
+// async function fetchData() {
+//   const result = await fetch('../data/mappingAcronyms.csv');
+//   const data = await result.text();
+//   return data;
+// }
 
-async function main() {
-    const acronym = await fetchData();
-    console.log(acronym); // This will be the CSV data as a string
+// async function main() {
+//   let acronym = await fetchData();
+//   console.log(acronym); // This will be the CSV data as a string
 
-    const gElement = document.getElementById('au'); // Replace 'au' with the actual id
-    const aElement = document.createElement('a');
-    aElement.setAttribute('href', 'https://example.com'); // Replace with your desired URL
+//   const gElement = document.getElementById('au'); // Replace 'au' with the actual id
+//   const aElement = document.createElement('a');
+//   aElement.setAttribute('xlink:title', 'Australia'); // Replace with your desired URL
+//   aElement.setAttribute('xlink:href', 'https://example.com'); // Replace with your desired URL
 
+//   while (gElement.firstChild) {
+//       aElement.appendChild(gElement.firstChild);
+//     }
+    
+//     // Append the <a> element to <g>
+//     gElement.appendChild(aElement);
 
-    while (gElement.firstChild) {
-        aElement.appendChild(gElement.firstChild);
-      }
-      
-      // Append the <a> element to <g>
-      gElement.appendChild(aElement);
+// }
 
-}
-
-main()
+// main()
 
 // parseCSVFile().then(() => {
 //     // You can access `parsedData` here, after the data has been parsed
